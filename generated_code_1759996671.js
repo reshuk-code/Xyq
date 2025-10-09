99a0fd04942d657bf5ffb36f55cdf7a1501bfbf4
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:42:53 +0545

    Auto-commit: generated_code_1759996671.js

---
+++ generated_code_1759996671.js
@@ -0,0 +1,29 @@
+```javascript
+// Simple random number guessing game in the console
+
+function guessTheNumber() {
+  const secretNumber = Math.floor(Math.random() * 100) + 1;
+  let guess = 0;
+  let attempts = 0;
+
+  while (guess !== secretNumber) {
+    guess = parseInt(prompt("Guess a number between 1 and 100:"));
+    attempts++;
+
+    if (isNaN(guess)) {
+      alert("Invalid input. Please enter a number.");
+      continue;
+    }
+
+    if (guess < secretNumber) {
+      alert("Too low!");
+    } else if (guess > secretNumber) {
+      alert("Too high!");
+    } else {
+      alert(`Congratulations! You guessed the number ${secretNumber} in ${attempts} attempts.`);
+    }
+  }
+}
+
+guessTheNumber();
+```
