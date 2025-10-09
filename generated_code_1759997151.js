00bf4fb4cb5638b48385f11903673b68a72f47b6
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:50:53 +0545

    Auto-commit: generated_code_1759997151.js

---
+++ generated_code_1759997151.js
@@ -0,0 +1,10 @@
+```javascript
+// Utility function to shuffle an array in place using Fisher-Yates shuffle
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+```
