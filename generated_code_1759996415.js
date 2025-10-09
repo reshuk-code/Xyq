b32d783356f3cf206b50ee8d61e93724485362fa
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:38:37 +0545

    Auto-commit: generated_code_1759996415.js

---
+++ generated_code_1759996415.js
@@ -0,0 +1,18 @@
+```javascript
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomColor();
+}
+
+// Call the function every 2 seconds
+setInterval(changeBackgroundColor, 2000);
+
+```
