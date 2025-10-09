22d106233ca4745771a2b23f5ea667149df855b1
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:47:40 +0545

    Auto-commit: generated_code_1759996958.js

---
+++ generated_code_1759996958.js
@@ -0,0 +1,12 @@
+```javascript
+// Simple Utility Function:  debounce
+function debounce(func, delay) {
+  let timeoutId;
+  return function(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+```
