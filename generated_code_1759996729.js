cc10226f54b183f9c3c35989e95e3b4de903f00a
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:43:51 +0545

    Auto-commit: generated_code_1759996729.js

---
+++ generated_code_1759996729.js
@@ -0,0 +1,9 @@
+```javascript
+function hexToRgb(hex) {
+  const bigint = parseInt(hex.slice(1), 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+```
