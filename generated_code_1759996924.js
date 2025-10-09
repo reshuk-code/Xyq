088303e2a7f8f54e486f41f978aa103de4ce1b57
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:47:06 +0545

    Auto-commit: generated_code_1759996924.js

---
+++ generated_code_1759996924.js
@@ -0,0 +1,13 @@
+```javascript
+function simpleCalculator(num1, num2, operator) {
+  switch (operator) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
+    case '/':
+      if (num2 === 0) return "Division by zero!";
+      return num1 / num2;
+    default: return "Invalid operator";
+  }
+}
+```
