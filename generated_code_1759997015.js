723bcc80d3f21975303f2e17a48a274d89cbc428
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:48:37 +0545

    Auto-commit: generated_code_1759997015.js

---
+++ generated_code_1759997015.js
@@ -0,0 +1,11 @@
+```javascript
+// Utility function to convert a hex color code to RGB.
+function hexToRgb(hex) {
+  hex = hex.replace(/^#/, '');
+  const bigint = parseInt(hex, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+```
