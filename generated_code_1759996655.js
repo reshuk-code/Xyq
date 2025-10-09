1ea0329e0555067a5ec47ced59d93e2604980bf8
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:42:36 +0545

    Auto-commit: generated_code_1759996655.js

---
+++ generated_code_1759996655.js
@@ -0,0 +1,32 @@
+```javascript
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function createColorBoxes(numBoxes) {
+  const container = document.createElement('div');
+  container.style.display = 'flex';
+  container.style.flexWrap = 'wrap';
+
+  for (let i = 0; i < numBoxes; i++) {
+    const box = document.createElement('div');
+    box.style.width = '50px';
+    box.style.height = '50px';
+    box.style.margin = '5px';
+    box.style.backgroundColor = getRandomColor();
+    box.addEventListener('click', function() {
+      this.style.backgroundColor = getRandomColor();
+    });
+    container.appendChild(box);
+  }
+
+  document.body.appendChild(container);
+}
+
+createColorBoxes(20);
+```
