58618afa08308e2d853da2bec44d945ec4eb9e2a
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:48:40 +0545

    Auto-commit: generated_code_1759997017.js

---
+++ generated_code_1759997017.js
@@ -0,0 +1,22 @@
+```javascript
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomColor();
+}
+
+// Change background color every 2 seconds
+setInterval(changeBackgroundColor, 2000);
+
+
+// Example usage in HTML (add this to your HTML file):
+// <button onclick="changeBackgroundColor()">Change Color</button>
+
+```
