182ec692c771df166b65025f22860e605c9232a4
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:46:10 +0545

    Auto-commit: generated_code_1759996868.js

---
+++ generated_code_1759996868.js
@@ -0,0 +1,12 @@
+```javascript
+// Simple utility function: converts Celsius to Fahrenheit
+
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+// Example usage (uncomment to test in a suitable environment like a browser console or Node.js)
+// console.log(celsiusToFahrenheit(0));   // Output: 32
+// console.log(celsiusToFahrenheit(100)); // Output: 212
+// console.log(celsiusToFahrenheit(25));  // Output: 77
+```
