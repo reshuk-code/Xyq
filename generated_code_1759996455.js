e15e93fc48cede7a1734404f604b29dd276c81e3
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:39:17 +0545

    Auto-commit: generated_code_1759996455.js

---
+++ generated_code_1759996455.js
@@ -0,0 +1,10 @@
+```javascript
+// Simple utility function: string reversal
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+// Example usage (for demonstration, not part of the function itself)
+// console.log(reverseString("hello")); // Output: olleh
+```
