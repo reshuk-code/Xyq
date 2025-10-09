058c64cc4ca32232bd6d93da3d626e1f350612c6
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:46:14 +0545

    Auto-commit: generated_code_1759996873.js

---
+++ generated_code_1759996873.js
@@ -0,0 +1,30 @@
+```javascript
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function createColorBoxes(numBoxes) {
+  const container = document.createElement('div');
+  container.style.display = 'flex';
+  container.style.flexWrap = 'wrap';
+  container.style.width = '400px';
+
+  for (let i = 0; i < numBoxes; i++) {
+    const box = document.createElement('div');
+    box.style.width = '50px';
+    box.style.height = '50px';
+    box.style.backgroundColor = getRandomColor();
+    box.style.margin = '5px';
+    container.appendChild(box);
+  }
+
+  document.body.appendChild(container);
+}
+
+createColorBoxes(20);
+```
