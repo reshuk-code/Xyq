1f88c0827c7c3b947210436df6ed5ffa8a0ef53a
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-09 13:47:25 +0545

    Auto-commit: generated_code_1759996943.js

---
+++ generated_code_1759996943.js
@@ -0,0 +1,34 @@
+```javascript
+function simpleCalculator() {
+  const num1 = parseFloat(prompt("Enter the first number:"));
+  const num2 = parseFloat(prompt("Enter the second number:"));
+  const operator = prompt("Enter the operator (+, -, *, /):");
+
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 === 0) {
+        result = "Cannot divide by zero";
+      } else {
+        result = num1 / num2;
+      }
+      break;
+    default:
+      result = "Invalid operator";
+  }
+
+  alert("Result: " + result);
+}
+
+//simpleCalculator();
+```
