1593743f9ac589bd9f7ed68e83189ff400424741
Reshuk Sapkota <reshuksapkota2007@gmail.com>
2025-10-10 14:55:10 +0545

    Auto-commit: generated_code_1760087408.js

---
+++ generated_code_1760087408.js
@@ -0,0 +1,10 @@
+```javascript
+// Simple utility function: currency formatter
+
+function formatCurrency(amount, currencyCode = 'USD', locale = 'en-US') {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currencyCode,
+  }).format(amount);
+}
+```
